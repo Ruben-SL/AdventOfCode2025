6f6c58dc99756be80c703c9c9f765845894b7ae0
Ruben Sluijter (RM) <AO608897@achmea.nl>
2025-12-04 11:00:22 +0100

    part one

--- day-2/index.ts
+++ day-2/index.ts
@@ -1,1 +1,30 @@
+import fs from 'fs';
+
 console.log('Advent of Code 2025 - Day 2');
+
+let answer = 0;
+
+fs.readFileSync('./day-2/input.txt', 'utf-8')
+  .split(',')
+  .forEach((line) => {
+    if (!line) return;
+    const inputIds = line
+      .replace('\r', '')
+      .split('-')
+      .map((part) => parseInt(part));
+
+    for (let i = inputIds[0]!; i <= inputIds[1]!; i++) {
+      const inputInString = i.toString();
+      if (inputInString.length % 2 !== 0) continue; //on odd length, continue
+
+      const sliceIndex = Math.floor(inputInString.length / 2);
+      const partOne = inputInString.slice(0, sliceIndex);
+      const parttwo = inputInString.slice(sliceIndex);
+
+      if (partOne === parttwo) {
+        console.log(i);
+        answer += i;
+      }
+    }
+  });
+console.log('Answer:', answer);
